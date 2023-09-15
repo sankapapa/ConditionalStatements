ad3020f69d6e557b968c2930a21b90aae31941df
sankapapa <pavankumar8485@gmail.com>
2023-09-15 21:32:31 +0530

    if-elseif

---
+++ ifelseif.java
@@ -0,0 +1,8 @@
+public class ifelseif {
+    public static void main(String[] args) {
+		int a=15;
+		if(a<10) {System.out.println("FIRST CONDITION PROVED");}
+		else if(a<20) {System.out.println("SECOND CONDITION PROVED");}
+		else {System.out.println(" BOTH CONDITIONS NOT PROVED");}
+}
+}
