31e5c38655d6b3c8b857adabc94be7c78c072955
sankapapa <pavankumar8485@gmail.com>
2023-09-15 21:16:58 +0530

    if-statement

---
+++ ifstatement.java
@@ -0,0 +1,9 @@
+public class ifstatement {
+    public static void main(String[] args) {
+		int a=10;
+		if (a==10) {
+			System.out.println(" CONDITION PROVED");
+		} else{System.out.println("CONDITION NOT PROVED");}
+	}
+    
+}
