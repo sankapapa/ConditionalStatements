31937fc54381411646feb034116b6b1f230a5b17
sankapapa <pavankumar8485@gmail.com>
2023-09-15 21:28:20 +0530

    if-else-if-ladder

---
+++ ifElseifLadder.java
@@ -0,0 +1,15 @@
+public class ifElseifLadder {
+    public static void main(String[] args) {
+		int a=11;
+		int b=15;
+		int c=20;
+		int d=37;
+		int e=45;
+	    if(a%2==0) {System.out.println("a=11 IS EVEN NUMBER");}
+		else if(b%2==0) {System.out.println(" 01:b=15 IS EVEN NUMBER");}
+		else if(c%2==0) {System.out.println("c=20 IS EVEN NUMBER");}
+		else if(d%2==0) {System.out.println("d=37 IS EVEN NUMBER");}
+		else if(e%2==0) {System.out.println("e=45 IS EVEN NUMBER");}
+		      else {System.out.println("EVEN NUMBER NOT FOUND");}
+	}
+}
